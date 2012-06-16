c5c0010f1fe757b0057d831ac1aa6e066d8d2898
Kozie <flamefingers@gmail.com>
2012-06-17 01:30:00 +0200

    Added try/catch block

--- src/com/kozie/dungeon/world/World.java
+++ src/com/kozie/dungeon/world/World.java
@@ -1,6 +1,7 @@
 package com.kozie.dungeon.world;
 
 import java.awt.image.BufferedImage;
+import java.io.IOException;
 import java.io.InputStream;
 import java.util.Vector;
 
@@ -14,14 +15,18 @@ public class World {
 	
 	public World(int level) {
 		
-		InputStream levelImgSrc = GameComponent.class.getResourceAsStream("/levels/"+level+".png");
-		BufferedImage levelImg = ImageIO.read(levelImgSrc);
-		
-		int width = levelImg.getWidth();
-		int height = levelImg.getHeight();
-		int[] pixels = levelImg.getRGB(0, 0, width, height, null, 0, width);
-		
-		tiles.setSize(pixels.length);
+		try {
+			InputStream levelImgSrc = GameComponent.class.getResourceAsStream("/levels/"+level+".png");
+			BufferedImage levelImg = ImageIO.read(levelImgSrc);
+			
+			int width = levelImg.getWidth();
+			int height = levelImg.getHeight();
+			int[] pixels = levelImg.getRGB(0, 0, width, height, null, 0, width);
+			
+			tiles.setSize(pixels.length);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 	
 	public WorldBlock getTileAt() {
